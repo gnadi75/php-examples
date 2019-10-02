4bcb51de96c5ac04778ca6f83ee2db58066e2f9f
gnadi75 <walter.gnadenberger75@gmail.com>
2019-10-02 13:29:43 +0000

    new test script env.sh

---
+++ env.php
@@ -0,0 +1,15 @@
+<?php
+// Boot datei
+require_once 'inc/boot.inc.php';
+require_once 'inc/config.inc.php';
+
+
+printf("env.php Page\n");
+printf("by Walter Gnadenberger\n");
+printf("------------------Environment--------------------\n");
+printf("DB_DSN=%s\n",$DB_DSN);
+$VAR1 = getenv("VAR1");
+
+printf("TEST_VARIABLE=%s\n",$VAR1);
+
+?>
---
+++ inc/config.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+// Konfigurationseinstellungen
+
+define('LDAP_SERVER','test.ldap.at');
+define('DB_DSN','testdb';
+define('DB_SERVER','servertestdb.myphoenix.at');
+$dbuser = getenv("MYSQL_USER");
+
+?>
