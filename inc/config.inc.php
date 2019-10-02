5e5e98ca74e7698b88dfd5707e334a644c3b4e83
gnadi75 <walter.gnadenberger75@gmail.com>
2019-10-02 13:33:05 +0000

    error in inc/config.inc.php

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -3,7 +3,7 @@
 // Konfigurationseinstellungen
 
 define('LDAP_SERVER','test.ldap.at');
-define('DB_DSN','testdb';
+define('DB_DSN','testdb');
 define('DB_SERVER','servertestdb.myphoenix.at');
 $dbuser = getenv("MYSQL_USER");
 
