ce63f1b160f282673b6fc4819c2bf340d34e26d2
gnadi75 <walter.gnadenberger75@gmail.com>
2019-10-02 14:03:47 +0000

    \n corrected

--- env.php
+++ env.php
@@ -4,8 +4,9 @@ require_once 'inc/boot.inc.php';
 require_once 'inc/config.inc.php';
 
 
-printf("env.php Page\n");
+printf("env.php Page" . "\n");
 printf("by Walter Gnadenberger\n");
+printf("\n");
 printf("------------------Environment--------------------\n");
 printf("DB_DSN=%s\n",$DB_DSN);
 $VAR1 = getenv("VAR1");
