da1865e36c4db0557cd26f1891c6ecea6b1f8725
gnadi75 <walter.gnadenberger75@gmail.com>
2019-10-02 12:38:17 +0000

    env variable, printf

--- dbtest.php
+++ dbtest.php
@@ -4,7 +4,8 @@ $dbport = getenv("MYSQL_SERVICE_PORT");
 $dbuser = getenv("MYSQL_USER");
 $dbpwd = getenv("MYSQL_PASSWORD");
 $dbname = getenv("databasename");
-printf("User=%s,Password=%s",$dbuser,$dbpwd);
+printf("User=%s,Password=%s,Database=%s,Host=%s,Port=%s\n",$dbuser,$dbpwd,$dbname,$dbhost,$dbport);
+$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
