dc6ffcd262faa0973669ab0d663761270e6700f2
gnadi75 <walter.gnadenberger75@gmail.com>
2019-10-02 14:15:48 +0000

    edit env.sh

--- env.php
+++ env.php
@@ -4,13 +4,20 @@ require_once 'inc/boot.inc.php';
 require_once 'inc/config.inc.php';
 
 
-printf("env.php Page" . "\n");
-printf("by Walter Gnadenberger\n");
+printf("env.php Page" . "\r\n");
+printf("by Walter Gnadenberger\r\n");
 printf("\n");
 printf("------------------Environment--------------------\n");
 printf("DB_DSN=%s\n",$DB_DSN);
 $VAR1 = getenv("VAR1");
 
 printf("TEST_VARIABLE=%s\n",$VAR1);
+
+
+echo "\r\n";
+echo "-------------------------------------------------------\r\n";
+everything = get_defined_vars();
+ksort($everything);
+print ("%s",$everything);
 
 ?>
